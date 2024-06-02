cdaab57c807463e3e9e2036dbea9594aa1e0d786
mirec <mirec84@gmail.com>
2024-06-02 11:42:23 +0200

    first commit

---
+++ streamlit_app_v1.py
@@ -0,0 +1,77 @@
+import streamlit as st
+from langchain_community.chat_models import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.globals import set_debug
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.output_parsers import StrOutputParser
+import os
+from langchain_community.chat_message_histories import SQLChatMessageHistory
+
+#set_debug(True)
+model_name = "llama3:latest"
+
+llm = ChatOllama(model=model_name)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You're an assistant who's good at {ability}"),
+    MessagesPlaceholder(variable_name="history"),
+    ("human", "{question}"),
+])
+
+chain = prompt | llm.bind(stop=["<|eot_id|>"]) | StrOutputParser()
+
+with_message_history = RunnableWithMessageHistory(
+    chain,
+    lambda session_id: SQLChatMessageHistory(
+        session_id=session_id, connection_string="sqlite:///sqlite.db"
+    ),
+    input_messages_key="question",
+    output_messages_key="output",
+    history_messages_key="history"
+)
+
+page_title = "🦙💬 Local Llama 3 Chatbot with Chat history"
+
+# App title
+st.set_page_config(page_title=page_title)
+
+
+with st.sidebar:
+    st.title(page_title)
+
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# User-provided prompt
+if user_input := st.chat_input():
+    st.session_state.messages = [{"role": "user", "content": user_input}]
+    with st.chat_message("user"):
+        st.write(user_input)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = with_message_history.stream(
+            {"ability": "everything", "question": user_input},
+            config={"configurable": {"session_id": os.getsid(0)}},
+            )
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
